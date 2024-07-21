ae58b2ed6f7481de54dd643855f5a62768f66ac3
aquilinerose <aquiline2003@gmail.com>
2024-07-21 23:10:59 +0530

    profile page

--- lib/presentation/events/events.dart
+++ lib/presentation/events/events.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:yess_app/presentation/core/constants.dart';
 
 class EventsPage extends StatelessWidget {
   const EventsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      color: Colors.white,
-      child: ListView(
-        children: [
-          const SizedBox(height: 10),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            child: Text(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            kHeight10,
+            Text(
               'Events',
               textAlign: TextAlign.left,
               style: Theme.of(context).textTheme.headlineMedium?.copyWith(
@@ -23,10 +23,7 @@ class EventsPage extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            child: Text(
+            Text(
               'Epic Event Linup',
               textAlign: TextAlign.left,
               style: Theme.of(context).textTheme.headlineMedium?.copyWith(
@@ -35,91 +32,85 @@ class EventsPage extends StatelessWidget {
                     fontFamily: 'Poppins',
                   ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'KE Auditorium',
-            textAlign: TextAlign.left,
-            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                  color: const Color(0xFF6761A3),
-                  fontSize: 20,
-                  fontFamily: 'Poppins',
-                ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: List.generate(
-                5, // Change this number to however many items you want
-                (index) => Container(
-                  padding: EdgeInsets.all(20),
-                  margin: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFEFEDFF),
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 0.5,
-                    ),
-                    borderRadius: BorderRadius.circular(
-                        20), // Add rounded corners if desired
+            kHeight20,
+            Text(
+              'KE Auditorium',
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    color: const Color(0xFF6761A3),
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
                   ),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.only(left: 10, right: 10),
-                          child: Text('Now happening'),
-                          decoration: BoxDecoration(
-                            color: Color(0xFFFEFFE2),
-                            border: Border.all(
-                              color: Color(0xFF41B4AE),
-                              width: 0.5,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  5, // Change this number to however many items you want
+                  (index) => Container(
+                    padding: EdgeInsets.all(20),
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFEFEDFF),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 0.5,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                          20), // Add rounded corners if desired
+                    ),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(left: 10, right: 10),
+                            child: Text('Now happening'),
+                            decoration: BoxDecoration(
+                              color: Color(0xFFFEFFE2),
+                              border: Border.all(
+                                color: Color(0xFF41B4AE),
+                                width: 0.5,
+                              ),
+                              borderRadius: BorderRadius.circular(30),
                             ),
-                            borderRadius: BorderRadius.circular(30),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'Catch The Flag',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'Poppins',
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
+                          kHeight10,
+                          Text(
+                            'Catch The Flag',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Poppins',
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
-                        ),
-                        Text(
-                          'lets cook your hacking skills',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'Poppins',
-                            fontSize: 15,
-                            fontWeight: FontWeight.normal,
+                          Text(
+                            'lets cook your hacking skills',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Poppins',
+                              fontSize: 15,
+                              fontWeight: FontWeight.normal,
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          '10:00 AM - 11:00 PM',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'Poppins',
-                            fontSize: 20,
-                            fontWeight: FontWeight.normal,
+                          kHeight10,
+                          Text(
+                            '10:00 AM - 11:00 PM',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Poppins',
+                              fontSize: 20,
+                              fontWeight: FontWeight.normal,
+                            ),
                           ),
-                        ),
-                      ]),
+                        ]),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-        ],
+            kHeight30,
+          ],
+        ),
       ),
     );
   }
--- lib/presentation/profile/profile.dart
+++ lib/presentation/profile/profile.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:yess_app/presentation/core/constants.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(20),
-        color: Colors.white,
-        child: Column(children: [
-          const SizedBox(height: 50),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            child: Text(
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: ListView(children: [
+            kHeight10,
+            Text(
               'Profile',
               textAlign: TextAlign.left,
               style: Theme.of(context).textTheme.headlineMedium?.copyWith(
@@ -22,7 +22,131 @@ class ProfilePage extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
             ),
-          ),
-        ]));
+            Container(
+              width: 100.0, // Width of the circle
+              height: 100.0, // Height of the circle
+              decoration: BoxDecoration(
+                color:
+                    Color.fromARGB(255, 152, 147, 214), // Color of the circle
+                shape: BoxShape.circle,
+              ),
+            ),
+            kHeight20,
+            Text(
+              'Name',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Poppins',
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              '+91 1234567890',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Poppins',
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              'yess@gmail.com',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Poppins',
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              'College of engineering',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Poppins',
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              'chengannur',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Poppins',
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              'student representative IEEE',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Poppins',
+                fontSize: 20,
+              ),
+            ),
+            kHeight20,
+            Container(
+              padding: EdgeInsets.all(20),
+              margin: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: const Color(0xFFEFEDFF),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 0.5,
+                ),
+                borderRadius:
+                    BorderRadius.circular(20), // Add rounded corners if desired
+              ),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Ticket',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Poppins',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    kHeight10,
+                    Text(
+                      'Name',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Poppins',
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    Text(
+                      'Ticket ID',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Poppins',
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ]),
+            ),
+            kHeight20,
+            Text(
+              'Upcoming Events',
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    color: const Color(0xFF6761A3),
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
+                  ),
+            ),
+          ]),
+        ));
   }
 }
